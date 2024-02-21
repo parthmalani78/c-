090bc086c99efa1f138e54f81f4e40b059da9991
Darshan <darshanchauhan5064@gmail.com>
2024-02-21 15:50:16 +0530

    cpp

---
+++ unary/expositivenegative.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+    int x,y;
+
+    public:
+        Complex(int a,int b)
+        {
+            x = a;
+            y = b;
+        }
+        void display()
+        {
+            cout<<"the real part:- "<<x<<endl;
+            cout<<"the imginary part:- "<<y<<endl;
+        }
+        Complex operator-() 
+        {
+            x= -x;
+            y= -y;
+        }
+};
+int main()
+{
+    Complex c1(-10,-15);
+    -c1;
+    c1.display();
+    return 0;
+}
